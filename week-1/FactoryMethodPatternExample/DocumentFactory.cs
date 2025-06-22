84b01906e0c7aef60a2453c66f19cec3616144ae
khushichouhan2314 <khushic2314@gmail.com>
2025-06-23 00:37:48 +0530

    Added Week 1 folder with project files

---
+++ week-1/FactoryMethodPatternExample/DocumentFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternExample
+{
+    public abstract class DocumentFactory
+    {
+        public abstract IDocument CreateDocument();
+    }
+
+    public class WordFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new WordDocument();
+        }
+    }
+
+    public class PdfFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new PdfDocument();
+        }
+    }
+
+    public class ExcelFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new ExcelDocument();
+        }
+    }
+
+}
---
+++ week-1/Financial Forecasting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double baseValue = 5000;
+        double growthRate = 0.05;
+        int years = 10;
+
+        double result = Forecast(baseValue, growthRate, years);
+        Console.WriteLine(result);
+    }
+
+    static double Forecast(double value, double rate, int years)
+    {
+        if (years == 0)
+            return value;
+        return Forecast(value, rate, years - 1) * (1 + rate);
+    }
+}
